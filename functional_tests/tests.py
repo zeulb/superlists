508b3ce15a21fdadcd959117e5692c1300ebc52f
Stefano Chiesa <base278@gmail.com>
2015-05-04 23:31:38 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome('/Users/zeulb/Dropbox/Programming/TDDPython/chromedriver')
@@ -79,7 +79,7 @@ class NewVisitorTest(LiveServerTestCase):
 			512,
 			delta=5
 		)
-		
+
 		#She starts a new list and sees the input is nicely centered there too
 		inputbox.send_keys('testing\n')
 		inputbox = self.browser.find_element_by_id('id_new_item')
