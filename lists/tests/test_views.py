760a0ed26ef6b4e3cea58e64fd0f76f4b96483f5
Stefano Chiesa <base278@gmail.com>
2015-05-15 23:53:07 +0700

    Handle uniqueness validation using django voodoo

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,6 +7,7 @@ from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_LIST_ERROR
 from django.http import HttpRequest
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -100,6 +101,19 @@ class ListViewTest(TestCase):
 	def test_for_invalid_input_shows_error_on_page(self):
 		response = self.post_invalid_input()
 		self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+	@skip
+	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+		list1 = List.objects.create()
+		item1 = Item.objects.create(list=list1, text='textey')
+		response = self.client.post(
+			'/lists/%d/' % (list1.id,),
+			data={'text': 'textey'}
+		)
+		expected_error = escape("You've already got this in your list")
+		self.assertContains(response, expected_error)
+		self.assertTemplateUsed(response, 'list.html')
+		self.assertEqual(Item.objects.all().count(), 1)
 
 
 
