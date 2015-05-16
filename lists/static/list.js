8f899bc93a04279111d2a2e60a51f578396a8107
Stefano Chiesa <base278@gmail.com>
2015-05-16 23:36:33 +0700

    Add a little javascript

---
+++ lists/static/list.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () {
+	$('input').on('keypress', function() {
+		$('.has-error').hide();	
+	})
+
+	$('input').on('click', function() {
+		$('.has-error').hide();	
+	})
+
+})
