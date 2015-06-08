9689232ce3ddca5813ba372611525021c7d65cb2
Stefano Chiesa <base278@gmail.com>
2015-06-08 17:02:19 -0500

    Custom persona auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,4 +86,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'superlists', 'static'),
+)
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
 )
