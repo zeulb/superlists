3df7aba92a575ab8e81df2b89be10b1d750d98dc
Stefano Chiesa <base278@gmail.com>
2015-06-08 22:57:17 -0500

    Placeholder test_my_lists and move login checkers into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,7 @@
 from .base import FunctionalTest
-from selenium.webdriver.support.ui import WebDriverWait
 import time
+
+TEST_EMAIL = 'edith@mockmyid.com'
 
 class LoginTest(FunctionalTest):
 	
@@ -15,11 +16,6 @@ class LoginTest(FunctionalTest):
 			time.sleep(0.5)
 		self.fail('could not find window')
 
-	def wait_for_element_with_id(self, element_id):
-		WebDriverWait(self.browser, timeout=30).until(
-			lambda b: b.find_element_by_id(element_id)
-		)
-
 	def test_login_with_persona(self):
 		# Edith goes to the awesome superlists site
 		# and notices a "Sign in" link for the first time.
@@ -33,13 +29,24 @@ class LoginTest(FunctionalTest):
 		## Use mockmyid.com for test email
 		self.browser.find_element_by_id(
 			'authentication_email'
-		).send_keys('edith@mockmyid.com')
+		).send_keys(TEST_EMAIL)
 		self.browser.find_element_by_tag_name('button').click()
 
 		# The Persona window closes
 		self.switch_to_new_window('To-Do')
 
 		# She can see that she is logged in
-		self.wait_for_element_with_id('id_logout')
-		navbar = self.browser.find_element_by_css_selector('.navbar')
-		self.assertIn('edith@mockmyid.com', navbar.text)
+		self.wait_to_be_logged_in(email=TEST_EMAIL)
+
+		# Refreshing the page, she sees it's a real session login,
+		# not just a one-off for that page
+		self.browser.refresh()
+		self.wait_to_be_logged_in(email=TEST_EMAIL)
+
+		# Terrified of this new feature, she reflexively clicks "logout"
+		self.browser.find_element_by_id('id_logout').click()
+		self.wait_to_be_logged_out(email=TEST_EMAIL)
+
+		# The "logged out" status also persists after a refresh
+		self.browser.refresh()
+		self.wait_to_be_logged_out(email=TEST_EMAIL)
