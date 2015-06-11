e2a3f9345af2d79f8c5511d5edeaf08a760aa79f
Stefano Chiesa <base278@gmail.com>
2015-06-11 17:25:30 -0500

    Add screenshot on failure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,6 +3,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 import sys
 from .server_tools import reset_database
+import os
+from datetime import datetime
+
+SCREEN_DUMP_LOCATION = os.path.join(
+	os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -30,7 +36,43 @@ class FunctionalTest(StaticLiveServerTestCase):
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
+		if self._test_has_failed():
+			if not os.path.exists(SCREEN_DUMP_LOCATION):
+				os.makedirs(SCREEN_DUMP_LOCATION)
+			for ix, handle in enumerate(self.browser.window_handles):
+				self._windowid = ix
+				self.browser.switch_to_window(handle)
+				self.take_screenshot()
+				self.dump_html()
 		self.browser.quit()
+		super().tearDown()
+
+	def _test_has_failed(self):
+		for method, error in self._outcome.errors:
+			if error:
+				return True
+		return False
+
+	def take_screenshot(self):
+		filename= self._get_filename() + '.png'
+		print('screenshooting to', filename)
+		self.browser.get_screenshoot_as_file(filename)
+
+	def dump_html(self):
+		filename= self._get_filename() + '.html'
+		print('dumping page HTML to', filename)
+		with open(filename, 'w') as f:
+			f.write(self.browser.page_source)
+
+	def _get_filename(self):
+		timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+		return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+			folder=SCREEN_DUMP_LOCATION,
+			classname=self.__class__.__name__,
+			method=self._testMethodName,
+			windowid=self._windowid,
+			timestamp=timestamp
+		)
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
